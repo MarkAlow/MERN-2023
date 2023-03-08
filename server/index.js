6065194bb3c6358aada0767789aef0cf744be3a7
MarkAlow <41274756+MarkAlow@users.noreply.github.com>
2023-03-08 15:39:34 -0500

    Redux

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://user:user123@cluster0.chr0zdi.mongodb.net/test";
