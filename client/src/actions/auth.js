7a3532a5928b3c923ee9fb09df53825cadebb398
MarkAlow <41274756+MarkAlow@users.noreply.github.com>
2023-03-11 12:46:50 -0500

    Continuing to work on auth permissions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -17,6 +17,10 @@ export const signin = (formData, history) => async (dispatch) => {
 export const signup = (formData, history) => async (dispatch) => {
   try {
     // sign up the user
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push("/");
   } catch (error) {
     console.log(error);
