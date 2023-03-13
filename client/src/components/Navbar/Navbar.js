74f5fe8712f5db83f5ecfa4602ce46e18439f3cc
MarkAlow <41274756+MarkAlow@users.noreply.github.com>
2023-03-13 01:06:35 -0400

    Visuals

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,8 @@ import { AppBar, Avatar, Typography, Toolbar, Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import decode from "jwt-decode";
 import useStyles from "./styles";
-import memories from "../../images/memories.png";
+import memoriesText from "../../images/memories-Text.png";
+import memoriesLogo from "../../images/memories-Logo.png";
 
 const Navbar = () => {
   const classes = useStyles();
@@ -36,23 +37,15 @@ const Navbar = () => {
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <div className={classes.brandContainer}>
-        <Typography
-          component={Link}
-          to="/"
-          className={classes.heading}
-          variant="h2"
-          align="center"
-        >
-          Memories
-        </Typography>
+      <Link to="/" className={classes.brandContainer}>
+        <img src={memoriesText} alt="icon" height="45px" />
         <img
           className={classes.image}
-          src={memories}
-          alt="memories"
-          height="40"
-        ></img>
-      </div>
+          src={memoriesLogo}
+          alt="icon"
+          height="40px"
+        />
+      </Link>
       <Toolbar className={classes.toolbar}>
         {user ? (
           <div className={classes.profile}>
